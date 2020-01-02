3efd76dc0a4f6803376f455f9beb1f645cafca11
kevinlokhandwala <59446374+kevinlokhandwala@users.noreply.github.com>
2020-01-02 16:58:11 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello and welcome to Java");
+		System.out.println("Hello and welcome to Java, made some changes");
 	}
 }
